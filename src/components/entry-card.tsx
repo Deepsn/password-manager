8fd6c417227a7810067a3e7975d0c4185373994f
Deepsn <75620590+Deepsn@users.noreply.github.com>
2025-08-15 11:43:06 -0300

    feat: add entry list with entry cards

--- src/components/entry-card.tsx
+++ src/components/entry-card.tsx
@@ -1,8 +1,77 @@
-export function EntryCard() {
+import {
+	EllipsisIcon,
+	KeyRoundIcon,
+	PencilIcon,
+	PinIcon,
+	Trash2Icon,
+} from "lucide-react";
+import { Badge } from "@/components/ui/badge";
+import {
+	Card,
+	CardAction,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+interface EntryCardProps {
+	name: string;
+	type: "password";
+}
+
+export function EntryCard({ name, type }: EntryCardProps) {
 	return (
-		<div className="w-full min-h-min p-4 rounded-lg shadow-md">
-			<h2>Entry Card</h2>
-			<p>This is a placeholder for the entry card component.</p>
-		</div>
+		<Card onClick={() => console.log("Card clicked")}>
+			<CardHeader>
+				<CardTitle>{name}</CardTitle>
+
+				<CardDescription>testemail@email.com</CardDescription>
+
+				<CardAction>
+					<DropdownMenu>
+						<DropdownMenuTrigger className="cursor-pointer">
+							<EllipsisIcon />
+						</DropdownMenuTrigger>
+						<DropdownMenuContent className="w-56" align="start">
+							<DropdownMenuLabel>Actions</DropdownMenuLabel>
+							<DropdownMenuSeparator />
+							<DropdownMenuItem>
+								<PencilIcon />
+								Edit
+							</DropdownMenuItem>
+							<DropdownMenuItem>
+								<PinIcon />
+								Pin
+							</DropdownMenuItem>
+							<DropdownMenuItem>
+								<Trash2Icon />
+								Delete
+							</DropdownMenuItem>
+						</DropdownMenuContent>
+					</DropdownMenu>
+				</CardAction>
+			</CardHeader>
+
+			<CardFooter>
+				{type === "password" && (
+					<Badge
+						variant={"secondary"}
+						className="bg-blue-500 text-white dark:bg-blue-600"
+					>
+						<KeyRoundIcon />
+						Password
+					</Badge>
+				)}
+			</CardFooter>
+		</Card>
 	);
 }
--- src/layout/entry-list.tsx
+++ src/layout/entry-list.tsx
@@ -3,9 +3,7 @@ import { EntryCard } from "../components/entry-card";
 export function EntryList() {
 	return (
 		<div className="flex flex-col gap-4 m-5">
-			<EntryCard />
-			<EntryCard />
-			<EntryCard />
+			<EntryCard name="TestEntry" type="password" />
 		</div>
 	);
 }
