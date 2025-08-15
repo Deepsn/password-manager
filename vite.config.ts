101e10ed655fe402a4dcbeea1bf310025955585e
Deepsn <75620590+Deepsn@users.noreply.github.com>
2025-08-15 00:09:03 -0300

    chore: add shadcn configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
-import { defineConfig } from "vite";
+import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "vite";
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [react(), tailwindcss()],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 
   // prevent vite from obscuring rust errors
   clearScreen: false,
