66e5a88d91d5f74fbb9ed657715ac8c7feabecd4
Deepsn <75620590+Deepsn@users.noreply.github.com>
2025-10-15 16:35:37 -0300

    feat: move ThemeProvider and add Toaster component to providers

--- src/providers/root-provider.tsx
+++ src/providers/root-provider.tsx
@@ -1,11 +1,13 @@
 import { Provider } from "jotai";
 import type { PropsWithChildren } from "react";
-import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 import { store } from "@/lib/jotai/store";
+import { ThemeProvider } from "@/providers/theme-provider";
 
 export function RootProvider({ children }: PropsWithChildren) {
 	return (
 		<ThemeProvider defaultTheme="system">
+			<Toaster />
 			<Provider store={store}>{children}</Provider>
 		</ThemeProvider>
 	);
